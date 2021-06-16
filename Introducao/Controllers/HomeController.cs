001e40a7ba6a354fcdc6c9d302a3934f9d1091e9
Wesley Silveira de Paula <wesleysdepaula@outlook.com>
2021-06-16 07:17:26 -0300

    Adicionar arquivos de projeto.

---
+++ Introducao/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Introducao.Models;
+
+namespace Introducao.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var pessoa = new Introducao.Models.Pessoa()
+            {
+                PessoaID = 1,
+                Nome = "Wesley Silveira",
+                Tipo = "Administrador"
+            };
+
+            return View(pessoa);
+        }
+
+        public ActionResult Contatos()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Lista(Pessoa pessoa)
+        {
+            return View(pessoa);
+        }
+    }
+}
